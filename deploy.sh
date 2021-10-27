650675de7c57cf68bfaf00142f4f7786bae75075
chewhx <chewhx@icloud.com>
2021-10-27 18:09:54 +0800

    added deploy sh

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+gcloud run deploy gcloud-helloworld-1 \
+      --region us-central1 \
+      --project gcloud-helloworld-315201 \
+      --image gcr.io/gcloud-helloworld-315201/gcloud-helloworld-1 \
